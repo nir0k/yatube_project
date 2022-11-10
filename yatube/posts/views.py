8ad26879eef41f4d9bfb095262fd062f5f865913
Ivan Korbakov <korbakovi@gmail.com>
2022-11-10 11:37:12 +0300

    finish theme 5 in 3 sprint

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,20 +1,25 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+# from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
-# Create your views here.
-def index(request):
-    template = 'posts/index.html'
-    title = 'Это главная страница проекта Yatube'
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    template = 'posts/group_list.html'
+    posts = Post.objects.filter(group=group).order_by('-pub_date')
     context = {
-        'title': title,
+        'group': group,
+        'posts': posts,
     }
     return render(request, template, context)
 
 
-def group_posts(request, slug):
-    title = 'Здесь будет информация о группах проекта Yatube'
+def index(request):
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'title': title,
+        'posts': posts,
     }
-    return HttpResponse(context['title'])
+    return render(request, template, context)
